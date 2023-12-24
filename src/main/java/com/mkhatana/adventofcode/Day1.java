7d5e2866ab8266afa448beeaf68414dad1208667
Mohammed Khatana <scm@mohammedkhatana.com>
2023-12-24 19:58:10 +0000

    feat: day 1 part 1 completed

--- src/main/java/com/mkhatana/adventofcode/Day1.java
+++ src/main/java/com/mkhatana/adventofcode/Day1.java
@@ -1,4 +1,37 @@
 package com.mkhatana.adventofcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day1 {
+
+    private Day1(){}
+
+    public static String filterDigits(String input) {
+        StringBuilder s = new StringBuilder();
+        for(Character c : input.toCharArray()) {
+            if(Character.isDigit(c)) {
+                s.append(c);
+            }
+        }
+        return s.toString();
+    }
+
+    public static int concatenateFirstAndLastDigit(String input) {
+        return Integer.valueOf(input.substring(0, 1) + input.substring(input.length() - 1, input.length()));
+    }
+
+    public static int sumCalibrations(List<Integer> calibrations) {
+        return calibrations.stream().reduce(0, (a,b)-> (a+b));
+    }
+
+    public static int completeChallenge(List<String> input) {
+        List<Integer> allIntegers = new ArrayList<>();
+        for (String line : input) {
+            String filterDigits = filterDigits(line);
+            int concatenated = concatenateFirstAndLastDigit(filterDigits);
+            allIntegers.add(concatenated);
+        }
+        return sumCalibrations(allIntegers);
+    }
 }
---
+++ src/test/java/com/mkhatana/adventofcode/Day1Test.java
@@ -0,0 +1,58 @@
+package com.mkhatana.adventofcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+class Day1Test {
+
+    @ParameterizedTest(name = "filters digits - #{arguments}")
+    @CsvSource({ "1abc2,12", "pqr3stu8vwx,38", "a1b2c3d4e5f,12345", "treb7uchet,7" })
+    void filtersDigits(String input, String expected) {
+        Assertions.assertEquals(expected, Day1.filterDigits(input));
+    }
+
+    @ParameterizedTest(name = "Concatenates first and last digits - #{arguments}")
+    @CsvSource({ "1abc2,12", "pqr3stu8vwx,38", "a1b2c3d4e5f,15", "treb7uchet,77" })
+    void concatenatesFirstAndLastDigit(String input, int expected) {
+        Assertions.assertEquals(expected, Day1.concatenateFirstAndLastDigit(Day1.filterDigits(input)));
+    }
+
+    @Test
+    void sumsAllCalibrations() {
+        List<Integer> calibrations = List.of(12, 38, 15, 77);
+        Assertions.assertEquals(142, Day1.sumCalibrations(calibrations));
+    }
+
+    @Test
+    void completesFirstPartChallenge() {
+        Assertions.assertEquals(142,
+                Day1.completeChallenge(List.of("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet")));
+    }
+
+    @Test
+    void outputsFirstPartChallengeTestResultFromInput() throws Exception {
+        Assertions.assertEquals(55017, Day1.completeChallenge(getInput()));
+    }
+
+    List<String> getInput() throws IOException {
+
+        ArrayList<String> list = new ArrayList<String>();
+        try (Stream<String> stream = Files.lines(Paths.get(Objects.requireNonNull(this.getClass()
+                .getClassLoader()
+                .getResource("Day1.input.txt")).getPath()))) {
+            stream.forEach(list::add);
+        }
+
+        return list;
+    }
+}
